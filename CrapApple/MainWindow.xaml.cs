3279579c057515e51e2cb3d347f7a350176c52e2
Harvey Walker <w22009720@northumbria.ac.uk>
2024-03-07 16:07:37 +0000

    Added hello world

--- CrapApple/MainWindow.xaml.cs
+++ CrapApple/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,6 +24,7 @@ namespace CrapApple
         public MainWindow()
         {
             InitializeComponent();
+            Debug.WriteLine("Hello World.");
         }
     }
 }
